3ab578c3d9e44a9e527aa672edc4ce8f1d44bf4f
BSIT3F_VASQUEZ <127589382+VASQUEZASHLEY@users.noreply.github.com>
2025-04-26 00:12:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 
 <html>
@@ -15,39 +18,55 @@
         <h1>Personal Budget Tracker!</h1>
         <hr />
 
-        <div class="login-form">
-          <h2>Login</h2>
+        <div class="container">
+            <?php if (isset($_SESSION['error'])): ?>
+                <div class="alert alert-danger">
+                    <?php 
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                </div>
+            <?php endif; ?>
+            
+            <?php if (isset($_SESSION['success'])): ?>
+                <div class="alert alert-success">
+                    <?php 
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                    ?>
+                </div>
+            <?php endif; ?>
 
-          <form method="post" action="server.php">
-          <div class="input-container">
-            <input type="email" class="input-field" placeholder="Email" name="email" required>
-          </div>
+            <div class="login-form">
+                <h2>Login</h2>
 
-          <div class="input-container">
-            <input type="password" class="input-field" placeholder="Password" name="password" required>
-          </div>
+                <form method="post" action="server.php">
+                <div class="input-container">
+                    <input type="email" class="input-field" placeholder="Email" name="email" required>
+                </div>
 
-          <div class="submit">
-            <button class="submit-button" name="login">Login</button>
-          </div>
+                <div class="input-container">
+                    <input type="password" class="input-field" placeholder="Password" name="password" required>
+                </div>
 
-          <div class="forgot-password">
-            <a href="forgot.html">Forgot Password?</a>
-          </div>
-        </div>
+                <div class="submit">
+                    <button class="submit-button" name="login">Login</button>
+                </div>
+
+            </div>
         
-          <hr />
 
-          <div class="register">
-            <p>Don't have an account? <a href="register.html">Register</a></p>
-          </div>
+                <div class="register">
+                    <p>Don't have an account? <a href="register.php">Register</a></p>
+                </div>
 
-          <div class="admin-login">
-            <a href="admin-login.php" class="admin-btn">Log-in as Admin</a>
-          </div>
+                <div class="admin-login">
+                    <a href="admin-login.php" class="admin-btn">Log-in as Admin</a>
+                </div>
 
-        </form>
-      </div>
+                </form>
+            </div>
+        </div>
 
 </body>
 
